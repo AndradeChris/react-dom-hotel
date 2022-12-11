72dc56fc5afdefeccfcd0d86f799b7582573295c
Debora Almeida <deboraalmeida89@gmail.com>
2022-12-10 23:05:54 -0300

    Hotfix eslint

--- src/components/atoms/GenericInput/index.js
+++ src/components/atoms/GenericInput/index.js
@@ -1,9 +1,19 @@
-import * as S from "./styles.js";
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as S from './styles.js'
 
-const GenericInput = ({type, placeholder, id, aName}) => {
-    return (
-        <S.InputTypes type={type} placeholder={placeholder} id={id} name={aName}></S.InputTypes>
-    );
-};
+const GenericInput = ({ type, placeholder, id, aName }) => {
+  return (
+        <S.InputTypes type={type} placeholder={placeholder} id={id} name={aName} />
 
-export default GenericInput;
+  )
+}
+
+export default GenericInput 
+
+GenericInput.propTypes = {
+  type: PropTypes.node,
+  placeholder: PropTypes.node,
+  id: PropTypes.node,
+  aName: PropTypes.node
+}
